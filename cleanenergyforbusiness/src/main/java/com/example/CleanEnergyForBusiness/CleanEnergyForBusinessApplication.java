ed1f515a5ff89cc31ad1712684b259ce3bf777ff
Amir-2112079 <2112079@brunel.ac.uk>
2023-01-07 18:47:23 +0000

    Updated all the files

---
+++ cleanenergyforbusiness/src/main/java/com/example/CleanEnergyForBusiness/CleanEnergyForBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.example.CleanEnergyForBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanEnergyForBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanEnergyForBusinessApplication.class, args);
+	}
+
+}
---
+++ cleanenergyforbusiness/src/test/java/com/example/CleanEnergyForBusiness/CleanEnergyForBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CleanEnergyForBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanEnergyForBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
