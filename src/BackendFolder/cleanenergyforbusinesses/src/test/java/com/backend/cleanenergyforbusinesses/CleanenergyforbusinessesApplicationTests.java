82541be03bb530f349911fcf50dfbc361094a58d
KorhanKahraman61 <2124579@brunel.ac.uk>
2023-03-02 17:37:20 +0000

    Backend update

---
+++ src/BackendFolder/cleanenergyforbusinesses/src/test/java/com/backend/cleanenergyforbusinesses/CleanenergyforbusinessesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cleanenergyforbusinesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanenergyforbusinessesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
